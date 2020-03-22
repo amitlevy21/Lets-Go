c21e34fca38cd67b1ed6cbdd6973e03557df0edb
Amit Levy <amitlevy347@gmail.com>
2020-03-22 17:33:39 -0500

    Format

--- vehicle_test.go
+++ vehicle_test.go
@@ -7,12 +7,12 @@ import (
 
 func TestCreateVehicle(t *testing.T) {
 	v := vehicle{
-		vCategory: bus,
-		manufactor: "Toyota",
+		vCategory:      bus,
+		manufactor:     "Toyota",
 		manufactorDate: time.Date(1999, time.April, 1, 19, 30, 50, 10, time.UTC),
-		activeSince: time.Date(2000, time.April, 1, 19, 30, 50, 10, time.UTC),
-		capacity: 30,
-		seats: 10,
+		activeSince:    time.Date(2000, time.April, 1, 19, 30, 50, 10, time.UTC),
+		capacity:       30,
+		seats:          10,
 	}
 	t.Logf("Vehicle created! %v", v)
 }
