115c2af5a2357f5cccc0fb28c09589939b87a23e
Amit Levy <amitlevy347@gmail.com>
2020-03-22 17:54:07 -0500

    Add ongoing ride data struct

---
+++ ongoing_ride_test.go
@@ -0,0 +1,16 @@
+package main
+
+import "testing"
+
+func TestCreateOngoingRide(t *testing.T) {
+	or := ongoingRide{
+		rStatus:        pending,
+		numPassengers:  0,
+		availableSeats: 30,
+		crewMembersIds: []int64{1, 2, 3},
+		rVehicle:       vehicle{},
+		latestStation:  station{},
+	}
+
+	t.Logf("Ongoing ride created! %v", or)
+}
