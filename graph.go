e37b73b4e2317f4df30e59e86cccce5244c726b5
Amit Levy <amitlevy347@gmail.com>
2020-04-07 20:03:26 -0500

    graph: Add Equal and DFS

--- graph.go
+++ graph.go
@@ -22,14 +22,84 @@ func NewGraph() *Graph {
 	return &Graph{simple.NewWeightedDirectedGraph(loopWeight, absentWeight)}
 }
 
-// BFS breadth-first-search algorithm, starts from given start node
-// invokes fn function when visiting every node, if fn return true
-// the function stops and returns true
-func (g *Graph) BFS(startNodeID int64, fn func(n graph.Node, depth int) bool) graph.Node {
+// BFS breadth-first-search algorithm, starts from given start node.
+// invokes fn function when visiting every node, stops early if until returns true.
+func (g *Graph) BFS(startNodeID int64, until func(n graph.Node, depth int) bool) graph.Node {
 	start := g.Node(startNodeID)
 	if start == nil {
 		return nil
 	}
 	BFS := traverse.BreadthFirst{}
-	return BFS.Walk(g, start, fn)
+	return BFS.Walk(g, start, until)
+}
+
+// DFS depth-first-search algorithm, starts from given start node.
+// invokes fn function when visiting every node, stops early if until returns true.
+func (g *Graph) DFS(startNodeID int64, until func(n graph.Node) bool) graph.Node {
+	start := g.Node(startNodeID)
+	if start == nil {
+		return nil
+	}
+	DFS := traverse.DepthFirst{}
+	return DFS.Walk(g, start, until)
+}
+
+// Equal check if g and o are equal, meaning, they have the same nodes and edges.
+// See EqualNodes and EqualEdges for more info.
+func (g *Graph) Equal(o *Graph) bool {
+	if g == o {
+		return true
+	}
+	if !g.EqualNodes(o) {
+		return false
+	}
+	if !g.EqualEdges(o) {
+		return false
+	}
+	return true
+}
+
+// EqualNodes checks if g and o has the same nodes IDs.
+func (g *Graph) EqualNodes(o *Graph) bool {
+	gn := g.Nodes()
+	on := o.Nodes()
+	for gn.Next() {
+		n1 := gn.Node().ID()
+		if !on.Next() {
+			return false
+		}
+		if n1 != on.Node().ID() {
+			return false
+		}
+	}
+	if on.Next() {
+		return false
+	}
+	return true
+}
+
+// EqualEdges checks if g and o have the same pointing edges with same weights.
+func (g *Graph) EqualEdges(o *Graph) bool {
+	ge := g.WeightedEdges()
+	oe := o.WeightedEdges()
+	for ge.Next() {
+		e1 := ge.WeightedEdge()
+		if !oe.Next() {
+			return false
+		}
+		e2 := oe.WeightedEdge()
+		if e1.From().ID() != e2.From().ID() {
+			return false
+		}
+		if e1.To().ID() != e2.To().ID() {
+			return false
+		}
+		if e1.Weight() != e2.Weight() {
+			return false
+		}
+	}
+	if oe.Next() {
+		return false
+	}
+	return true
 }
--- graph_test.go
+++ graph_test.go
@@ -46,3 +46,128 @@ func TestBFSEarlyStop(t *testing.T) {
 		t.Error("did not iterate before early stopping")
 	}
 }
+
+func TestDFSNonExistSrc(t *testing.T) {
+	g := NewGraph()
+	if node := g.DFS(1, func(v graph.Node) bool { return true }); node != nil {
+		t.Error("should not DFS when src node not in graph")
+	}
+}
+
+func TestDFS(t *testing.T) {
+	g := NewGraph()
+	g.AddNode(g.NewNode())
+	if node := g.DFS(0, func(v graph.Node) bool { return false }); node != nil {
+		t.Error("should DFS on one node")
+	}
+}
+
+func TestEqualSelf(t *testing.T) {
+	g1 := NewGraph()
+	if !g1.Equal(g1) {
+		t.Errorf("empty graphs should equal")
+	}
+}
+
+func TestEqualEmpty(t *testing.T) {
+	g1 := NewGraph()
+	g2 := NewGraph()
+	if !g1.Equal(g2) || !g2.Equal(g1) {
+		t.Errorf("empty graphs should equal")
+	}
+}
+
+func TestNotEqualNodes(t *testing.T) {
+	g1 := NewGraph()
+	g2 := NewGraph()
+	g1.AddNode(g1.NewNode())
+	if g1.Equal(g2) || g2.Equal(g1) {
+		t.Errorf("graphs should not equal")
+	}
+}
+
+func TestEqualNodes(t *testing.T) {
+	g1 := NewGraph()
+	g2 := NewGraph()
+	g1.AddNode(g1.NewNode())
+	g2.AddNode(g2.NewNode())
+	if !g1.Equal(g2) || !g2.Equal(g1) {
+		t.Errorf("graphs should equal")
+	}
+}
+
+func TestNotEqualEdges(t *testing.T) {
+	g1 := NewGraph()
+	g2 := NewGraph()
+	n1 := g1.NewNode()
+	g1.AddNode(n1)
+	n2 := g1.NewNode()
+	g1.AddNode(n2)
+	n1 = g2.NewNode()
+	g2.AddNode(n1)
+	n2 = g2.NewNode()
+	g2.AddNode(n2)
+	g1.SetWeightedEdge(g1.NewWeightedEdge(n1, n2, 0))
+	if g1.Equal(g2) || g2.Equal(g1) {
+		t.Errorf("g1 should not equal g2")
+	}
+}
+
+func TestEqualEdgesToDiffNodes(t *testing.T) {
+	g1 := NewGraph()
+	g2 := NewGraph()
+	n1 := g1.NewNode()
+	g1.AddNode(n1)
+	n2 := g1.NewNode()
+	g1.AddNode(n2)
+	g1.SetWeightedEdge(g1.NewWeightedEdge(n1, n2, 0))
+	n1 = g2.NewNode()
+	g2.AddNode(n1)
+	n2 = g2.NewNode()
+	g2.AddNode(n2)
+	g2.SetWeightedEdge(g2.NewWeightedEdge(n2, n1, 0))
+	if g1.Equal(g2) || g2.Equal(g1) {
+		t.Errorf("graphs should not equal")
+	}
+}
+
+
+func TestEqualEdgesToDiffNodes2(t *testing.T) {
+	g1 := NewGraph()
+	g2 := NewGraph()
+	n1 := g1.NewNode()
+	g1.AddNode(n1)
+	n2 := g1.NewNode()
+	g1.AddNode(n2)
+	n3 := g1.NewNode()
+	g1.AddNode(n3)
+	g1.SetWeightedEdge(g1.NewWeightedEdge(n1, n2, 0))
+	n1 = g2.NewNode()
+	g2.AddNode(n1)
+	n2 = g2.NewNode()
+	g2.AddNode(n2)
+	n3 = g2.NewNode()
+	g2.AddNode(n3)
+	g2.SetWeightedEdge(g2.NewWeightedEdge(n1, n3, 0))
+	if g1.Equal(g2) || g2.Equal(g1) {
+		t.Errorf("graphs should not equal")
+	}
+}
+
+func TestEqualEdgesDiffWeight(t *testing.T) {
+	g1 := NewGraph()
+	g2 := NewGraph()
+	n1 := g1.NewNode()
+	g1.AddNode(n1)
+	n2 := g1.NewNode()
+	g1.AddNode(n2)
+	g1.SetWeightedEdge(g1.NewWeightedEdge(n1, n2, 0))
+	n1 = g2.NewNode()
+	g2.AddNode(n1)
+	n2 = g2.NewNode()
+	g2.AddNode(n2)
+	g2.SetWeightedEdge(g2.NewWeightedEdge(n1, n2, 1))
+	if g1.Equal(g2) || g2.Equal(g1) {
+		t.Errorf("graphs should not equal")
+	}
+}
